f06ce686d6203e15a6304acc4b9ba5bd78f7ed4c
Harshit Jaiswal <harshitjaiswal1809@gmail.com>
2018-08-09 23:23:33 +0530

    Update myTemplate.cpp

--- myTemplate.cpp
+++ myTemplate.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <typeinfo>
+#include <set>
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define all(c) c.begin(),c.end()
+#define present(container,element) (container.find(element)!=container.end())
+
 
 using namespace std;
 
-void f(vector <int> &A) {
-    A[0]=45;
-    vector <int> :: iterator it=A.begin();
-    cout << (*it) << endl;
-}
-
 int main() {
-    vector <int> A;
-    for(int i=0;i<8;i++) {
-        A.push_back(i*i);
-    }
-    
-    vector <int> :: iterator it1=A.begin(),it2=A.begin();
-    it1+=4;
-    it2+=6;
+    // vector <int> :: iterator it1=A.begin(),it2=A.begin();
+    // it1+=4;
+    // it2+=6;
 //     int n=it2-it1; // subtract the position of iterators
 //     cout << n << endl;
     
@@ -30,12 +21,26 @@ int main() {
 //     it2 = max_element(all(A)); // return iterator of the max element
 //     cout << it2-A.begin() << endl; // to find the position 
     // f(A); use of const
-    A.insert(1,42); // shift every element after index 1 & insert 42 @ second position
+    // A.insert(1,42); // shift every element after index 1 & insert 42 @ second position
     
     // also have erase funtion
     // erase (it);
     // erase (it begin,it end)
+    set <int> s;
+    s.insert(2);
+    s.insert(1);
+    s.insert(8);
+    // cout << int(s.size());
     
+    // dont use global find instead use member function ::find()
+    if(s.find(2)!=s.end()) cout << "Yes";
+    else cout << "No";
+    
+    if(s.count(2)!=0) cout << "Yes"; ) //(log n) / O(n)
+    else cout << "No"; // O(log n)
+    
+    
+    // by creating vector from map we can use the indies as we cannot able to do in map!
     
     
     
